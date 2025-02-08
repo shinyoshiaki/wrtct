8164dd3fc3902839d3018d1568d7ee4a7d7d95cb
shinyoshiaki <shinyoshiaki2012@gmail.com>
2025-02-08 23:49:10 +0900

    Refactor WebRTC connection handling and update ICE candidate management

--- examples/mediachannel/sendrecv/answer.ts
+++ examples/mediachannel/sendrecv/answer.ts
@@ -8,31 +8,33 @@ const server = new Server({ port: 8888 });
 console.log("start");
 
 server.on("connection", (socket) => {
+  const pc = new RTCPeerConnection({
+    iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+    headerExtensions: {
+      video: [useSdesRTPStreamId()],
+      audio: [],
+    },
+  });
+  pc.onconnectionstatechange = () => {
+    console.log("connection state", pc.connectionState);
+  };
+  pc.oniceconnectionstatechange = () => {
+    console.log("ice connection state", pc.iceConnectionState);
+  };
+
+  const transceiver = pc.addTransceiver("video", { direction: "recvonly" });
+  transceiver.onTrack.subscribe((track, transceiver) => {
+    transceiver.sender.replaceTrack(track);
+  });
+
   socket.on("message", async (data) => {
-    const offer = JSON.parse(data as string);
-
-    const pc = new RTCPeerConnection({
-      iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
-      headerExtensions: {
-        video: [useSdesRTPStreamId()],
-        audio: [],
-      },
-    });
-    pc.onconnectionstatechange = () => {
-      console.log("connection state", pc.connectionState);
-    };
-    pc.oniceconnectionstatechange = () => {
-      console.log("ice connection state", pc.iceConnectionState);
-    };
-
-    const transceiver = pc.addTransceiver("video", { direction: "recvonly" });
-
-    transceiver.onTrack.subscribe((track, transceiver) => {
-      transceiver.sender.replaceTrack(track);
-    });
-
-    await pc.setRemoteDescription(offer);
-    await pc.setLocalDescription(await pc.createAnswer());
-    socket.send(JSON.stringify(pc.localDescription));
+    const msg = JSON.parse(data as string);
+    if (msg.candidate) {
+      pc.addIceCandidate(msg);
+    } else {
+      await pc.setRemoteDescription(msg);
+      await pc.setLocalDescription(await pc.createAnswer());
+      socket.send(JSON.stringify(pc.localDescription));
+    }
   });
 });
